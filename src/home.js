ff0019997c9bc3ef3ba493c569bfa5b90da62419
Robinee <yb6925@naver.com>
2023-04-12 21:57:34 +0900

    Create 'createElement'function and write a code to render a nav

--- src/home.js
+++ src/home.js
@@ -1,1 +1,36 @@
-function loadHomePage() {}
+function createElement(type) {
+  return document.createElement(type);
+}
+function loadHomePage() {
+  //1.render nav
+  /**
+   * structure of nav
+   * nav.nav > ul > li[class="link"]
+   */
+  const nav = createElement('nav');
+  const ul = createElement('ul');
+  let navItems = ['Home', 'Menu', 'Contact'];
+
+  navItems = navItems.map((item) => {
+    const li = createElement('li');
+    li.textContent = item;
+    li.classList.add('list');
+    return li;
+  });
+  ul.append(...navItems);
+  nav.append(ul);
+  console.log(nav);
+
+  //2.render header
+  /**
+   * structure of header
+   * header > div.container> h1 + p
+   */
+
+  //3.render footer
+  /**
+   * footer > p
+   */
+}
+
+export { loadHomePage };
