e87a748fb714b36df6adb69acba41606ea698eb7
Robinee <yb6925@naver.com>
2023-04-12 15:50:20 +0900

    Adjust to use webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js', //central point to make dependency graph
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'main.js', //output filename
     path: path.resolve(__dirname, 'dist'), //output location
