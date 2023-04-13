6db112eee68c280070d4c0a118d1dcb1839a8928
Robinee <yb6925@naver.com>
2023-04-13 20:36:45 +0900

    Add import createElement and remove createElement declared in file

--- src/home.js
+++ src/home.js
@@ -1,14 +1,4 @@
-function createElement(type, className, text) {
-  const newElement = document.createElement(type);
-
-  if (className || text) {
-    //if there is a need to set class or text
-    if (className) newElement.classList.add(className);
-    if (text) newElement.textContent = text;
-  }
-
-  return newElement;
-}
+import { createElement } from './createElement.js';
 
 function loadHomePage() {
   //1.render nav
