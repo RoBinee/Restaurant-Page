d802afbab9994b404162315ebe58df53ed44640e
Robinee <yb6925@naver.com>
2023-04-13 21:19:17 +0900

    Add style.css import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+import './style.css';
 import { loadHomePage } from './home.js';
+
 /**
  *  appending each new element to div#content once the page is first loaded.
  * write this initial page-load function inside of its own module and then import and call it inside of index.js.
