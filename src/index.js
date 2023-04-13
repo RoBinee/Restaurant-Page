41f07b8235dd299564d4558c12ea6966630dfd04
Robinee <yb6925@naver.com>
2023-04-13 20:47:58 +0900

    Remove all the code and call loadHomePage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import { loadHomePage } from './home.js';
  *  appending each new element to div#content once the page is first loaded.
  * write this initial page-load function inside of its own module and then import and call it inside of index.js.
  */
-const content = document.getElementById('content');
-const { nav, header, footer } = loadHomePage();
-content.append(nav, header, footer);
+
+loadHomePage();
