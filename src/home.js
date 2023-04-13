728912898dc7401c1521da0c61a1f3dc86fbccf0
Robinee <yb6925@naver.com>
2023-04-13 21:32:53 +0900

    Add addEventListener for each li element

--- src/home.js
+++ src/home.js
@@ -1,9 +1,24 @@
 import { nav } from './nav.js';
 import { header } from './header.js';
 import { footer } from './footer.js';
+
 function loadHomePage() {
   const content = document.getElementById('content');
   content.append(nav, header, footer);
+
+  const lists = document.querySelectorAll('li');
+  lists.forEach((list) => {
+    list.addEventListener('click', (e) => {
+      const menu = e.currentTarget.textContent;
+      if (menu === 'Home') {
+        console.log('render homepage');
+      } else if (menu === 'Menu') {
+        console.log('render menu page');
+      } else if (menu === 'Contact') {
+        console.log('render contact page');
+      }
+    });
+  });
 }
 
 export { loadHomePage };
