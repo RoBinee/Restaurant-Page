3fe6febad3261f1a35b512f80d95d95aaff94bc9
Robinee <yb6925@naver.com>
2023-04-13 17:37:44 +0900

    Rewrite the code using function expression

--- src/home.js
+++ src/home.js
@@ -16,41 +16,48 @@ function loadHomePage() {
    * structure of nav
    * nav.nav > ul > li[class="link"]
    */
-  const nav = createElement('nav', 'nav');
-  const ul = createElement('ul');
-  let navItems = ['Home', 'Menu', 'Contact'];
+  const nav = (function () {
+    const nav = createElement('nav', 'nav');
+    const ul = createElement('ul');
+    let navItems = ['Home', 'Menu', 'Contact'];
 
-  navItems = navItems.map((item) => {
-    return createElement('li', 'list', item);
-  });
-  ul.append(...navItems);
-  nav.append(ul);
-  //after append, navItems, ul variables are useless
+    navItems = navItems.map((item) => {
+      return createElement('li', 'list', item);
+    });
+    ul.append(...navItems);
+    nav.append(ul);
+    return nav;
+  })();
 
   //2.render header
   /**
    * structure of header
    * header > div.container> h1 + p
    */
-  const header = createElement('header');
-  const container = createElement('div', 'container');
-  container.innerHTML = `<h1>Restaurant Page</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
-      voluptatum expedita tempore eaque at numquam. Sequi iste voluptatem
-      ducimus nulla.
-    </p>`;
-  header.append(container);
-  //after append, container variable is useless
+  const header = (function () {
+    const header = createElement('header');
+    const container = createElement('div', 'container');
+    container.innerHTML = `<h1>Restaurant Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+        voluptatum expedita tempore eaque at numquam. Sequi iste voluptatem
+        ducimus nulla.
+      </p>`;
+    header.append(container);
+    return header;
+  })();
 
   //3.render footer
   /**
    * footer > p
    */
-  const footer = createElement('footer', 'footer');
-  const para = createElement('p', undefined, 'Copyright © 2023 Robinee');
-  footer.append(para);
-  //after append, para variable is useless
+  const footer = (function () {
+    const footer = createElement('footer', 'footer');
+    const para = createElement('p', undefined, 'Copyright © 2023 Robinee');
+    footer.append(para);
+    return footer;
+  })();
+  console.log(footer);
 
   return { nav, header, footer };
 }
