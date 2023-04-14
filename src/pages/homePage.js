ffd17adcf098ed2890aac4ccfd637e351c8a2739
Robinee <yb6925@naver.com>
2023-04-15 00:18:58 +0900

    Add structure

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,2 +1,13 @@
 //Home page
 //first make HTML structure of menu page
+
+// structure
+
+/* <div id="homePage">
+  <h1 class="title">Plex Restaurant</h1>
+    <p class="intro">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+      voluptatum expedita tempore eaque at numquam. Sequi iste voluptatem
+      ducimus nulla.
+    </p>
+</div> */
