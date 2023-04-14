eaee017acef73f6a79881e00d5038a7ba2362ee2
Robinee <yb6925@naver.com>
2023-04-15 00:17:32 +0900

    Addd structure

--- src/pages/menuPage.js
+++ src/pages/menuPage.js
@@ -1,2 +1,23 @@
 //menu page
 //first make HTML structure of menu page
+
+// structure of menuPage
+/* <div id="menuPage">
+<h2 class="sub-title">Menu</h2>
+<section class="menu-content">
+  <article class="menu-card">
+    <img
+      src="../src/images/cocktail.jpg"
+      alt=""
+      class="menu-img"
+    />
+    <div class="menu-desc">
+      <div>
+        <h3 class="menu-name">Cocktail</h3>
+        <h3 class="menu-price">9$</h3>
+      </div>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  </article>
+</section>
+</div> */
