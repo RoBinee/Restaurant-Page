61a9a4e3ea57a5236f7a1c335bcf3d2a09082a1e
Robinee <yb6925@naver.com>
2023-04-15 00:34:24 +0900

    Add structure

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -1,2 +1,17 @@
 //contact page
 //first make HTML structure of contact page
+
+// structure of aboutpage
+/* <div id="aboutPage">
+<h2 class="sub-title">About</h2>
+<h3>Lorem ipsum dolor sit amet.</h3>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+  eaque cumque nihil ipsam a deleniti hic fuga nesciunt dolorum
+  iure, aspernatur velit minus ex facilis. Animi aliquid excepturi
+  vitae iste! Lorem ipsum dolor sit amet consectetur adipisicing
+  elit. Illum in itaque similique id perferendis vero fugit
+  accusamus vel veniam facilis minus nostrum dicta adipisci alias,
+  placeat debitis nemo possimus at.
+</p>
+</div> */
