35349a106a8f25aa5640f94575d8c5fc374805eb
Robinee <yb6925@naver.com>
2023-04-14 23:32:03 +0900

    Add new images for menu

--- src/header.js
+++ src/header.js
@@ -3,12 +3,14 @@ import { createElement } from './createElement.js';
 export const header = (function () {
   const header = createElement('header');
   const container = createElement('div', 'container');
-  container.innerHTML = `<h1>Restaurant Page</h1>
-      <p>
+  container.innerHTML = `<div id="homePage">
+    <h1 class="title">Restaurant Page</h1>
+      <p class="intro">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
         voluptatum expedita tempore eaque at numquam. Sequi iste voluptatem
         ducimus nulla.
-      </p>`;
+      </p>
+  </div>`;
   header.append(container);
   return header;
 })();
