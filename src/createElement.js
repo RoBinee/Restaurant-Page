61cbdb938450a275aed5f8dfde0e3afcf95c42e1
Robinee <yb6925@naver.com>
2023-04-15 10:15:38 +0900

    Set id

--- src/createElement.js
+++ src/createElement.js
@@ -1,10 +1,11 @@
-export function createElement(type, className, text) {
+export function createElement(type, className, text, id) {
   const newElement = document.createElement(type);
 
   if (className || text) {
     //if there is a need to set class or text
     if (className) newElement.classList.add(className);
     if (text) newElement.textContent = text;
+    if (id) newElement.id = id;
   }
 
   return newElement;
