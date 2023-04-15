e990e3e97fc55a620e1683004064bf2e0a77c29b
Robinee <yb6925@naver.com>
2023-04-15 13:50:10 +0900

    Create components folder

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import { createElement } from '../createElement.js';
+import { homeContent } from '../pages/homePage.js';
+export const header = (function () {
+  const header = createElement('header');
+  const container = createElement('div', 'container');
+  container.append(homeContent);
+  header.append(container);
+  return header;
+})();
