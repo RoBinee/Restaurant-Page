f0878285f84f778e74e875b67e2cfd52a1e4ac6c
Robinee <yb6925@naver.com>
2023-04-15 15:38:09 +0900

    Add eventListener for renderPage func

--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
-import background from './images/background.jpg';
 import './style.css';
 import { renderPage } from './initialPage.js';
 import { homeContent } from './pages/homePage.js';
 import { menuContent } from './pages/menuPage.js';
 import { aboutContent } from './pages/aboutPage.js';
-/**
- *  appending each new element to div#content once the page is first loaded.
- * write this initial page-load function inside of its own module and then import and call it inside of index.js.
- */
 
-renderPage();
+window.addEventListener('DOMContentLoaded', renderPage);
 
 const container = document.querySelector('.container');
-//insert menu content into container
 const lists = document.querySelectorAll('li');
 lists.forEach((list) => {
   list.addEventListener('click', (e) => {
     const menu = e.currentTarget.textContent;
     if (menu === 'Home') {
-      console.log('render homepage');
-      container.innerHTML = homeContent;
+      container.removeChild(container.firstChild);
+      container.appendChild(homeContent);
     } else if (menu === 'Menu') {
-      console.log('render menu page');
-      container.innerHTML = menuContent;
+      container.removeChild(container.firstChild);
+      container.appendChild(menuContent);
     } else if (menu === 'About') {
-      console.log('render About page');
-      container.innerHTML = aboutContent;
+      container.removeChild(container.firstChild);
+      container.appendChild(aboutContent);
     }
   });
 });
